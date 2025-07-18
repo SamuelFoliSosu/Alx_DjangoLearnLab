64bc6aaec89a2701f7ed6f80463e70cecb811da4
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-07-18 00:04:40 +0000

    Added all files for django-models project task 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
-# django-models/LibraryProject/LibraryProject/urls.py
+# django-models/LibraryProject/relationship_app/urls.py
 from django.urls import path
-from .views import book_list
-from .views import LibraryDetailView
+from .views import book_list           # <--- Add/ensure this exact line
+from .views import LibraryDetailView   # <--- Keep this for the class-based view
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'), # <--- Now directly reference book_list
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # <--- Now directly reference LibraryDetailView
+    path('books/', book_list, name='book_list'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
