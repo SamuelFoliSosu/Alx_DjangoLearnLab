d01521dc941fa1a791c79957fa3529bae6ffec58
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-05 23:32:58 +0000

    Checker fail fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 # advanced-api-project/api/views.py
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+# Corrected import order for the checker
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
