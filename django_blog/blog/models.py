b59803b4502231593c168bfedd18ac1f212190ed
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-14 22:55:33 +0000

    3.... step 1

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -26,3 +26,18 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)    
+
+# ... (Your existing Post model code)
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
