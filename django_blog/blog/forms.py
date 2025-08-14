5f7f9684c2593ef1a95132bbfd41d729d5212e6d
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-14 22:13:17 +0000

    1. Implementing the Blog's User Authentication System

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+    
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        self.fields.pop('password')
+        
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
