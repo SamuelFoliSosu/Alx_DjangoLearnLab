ebc45b181e3e97dc69ec161561445fa5ec2c7370
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-20 22:57:25 +0000

    corrected checker fail in task 2

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,7 +1,7 @@
 # posts/serializers.py
 
 from rest_framework import serializers
-from .models import Post, Comment
+from .models import Post, Comment, Like
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
@@ -11,9 +11,16 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'author', 'content', 'created_at']
         read_only_fields = ['created_at']
 
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
+        read_only_fields = ['created_at']
+
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,15 @@
 # posts/views.py
 
-from rest_framework import viewsets,generics
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from .permissions import IsAuthorOrReadOnly
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -32,3 +36,27 @@ class FeedView(generics.ListAPIView):
         followed_users = self.request.user.following.all()
         # Get posts from those users, ordered by creation date
         return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    
+class LikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        user = request.user
+        like, created = Like.objects.get_or_create(user=user, post=post)
+        if not created:
+            like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        # Optionally, create a notification here
+        # from notifications.models import Notification
+        # from django.contrib.contenttypes.models import ContentType
+        # content_type = ContentType.objects.get_for_model(post)
+        # Notification.objects.create(
+        #    recipient=post.author,
+        #    actor=user,
+        #    verb="liked",
+        #    target=post
+        # )
+
+        return Response(status=status.HTTP_201_CREATED)
