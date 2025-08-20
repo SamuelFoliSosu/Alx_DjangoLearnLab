4621e1c39c78f1e364e21213cc073a708aaed011
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-20 23:21:36 +0000

    corrected checker fail in task 2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/<str:username>/', UserProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),    # New URL
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'), # New URL
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),    # New URL
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'), # New URL
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -35,8 +35,8 @@ class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk):
-        user_to_follow = get_object_or_404(self.get_queryset(), pk=pk)
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(self.get_queryset(), pk=user_id)
         if request.user == user_to_follow:
             return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
         
@@ -47,7 +47,7 @@ class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk):
-        user_to_unfollow = get_object_or_404(self.get_queryset(), pk=pk)
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(self.get_queryset(), pk=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({'status': 'You have unfollowed this user.'}, status=status.HTTP_200_OK)
