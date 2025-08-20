ec821dabea283e68de33b093486a6af57fbabb16
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-08-20 23:59:44 +0000

    3. Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,18 +1,17 @@
-# required by checker: permissions.IsAuthenticated
-## posts/views.py
+# posts/views.py
 
-from rest_framework import viewsets, generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics, status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
 from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from .permissions import IsAuthorOrReadOnly
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from notifications.models import Notification # Add this import
-from django.contrib.contenttypes.models import ContentType
+from notifications.models import Notification
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -35,11 +34,9 @@ class FeedView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Get the users that the current user is following
         following_users = self.request.user.following.all()
-        # Get posts from those users, ordered by creation date
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
-    
+
 class LikeView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -47,17 +44,13 @@ class LikeView(APIView):
         post = get_object_or_404(Post, pk=pk)
         user = request.user
         
-        # This line is checked
         like, created = Like.objects.get_or_create(user=user, post=post)
         
         if not created:
-            # If the like already existed, it means the user is unliking the post
             like.delete()
             return Response({'status': 'unliked'}, status=status.HTTP_204_NO_CONTENT)
         
-        # This line is checked
-        # Only create a notification if the user successfully liked the post
-        if post.author != user: # Don't send a notification if a user likes their own post
+        if post.author != user:
             Notification.objects.create(
                 recipient=post.author,
                 actor=user,
@@ -66,3 +59,17 @@ class LikeView(APIView):
             )
         
         return Response({'status': 'liked'}, status=status.HTTP_201_CREATED)
+
+class SearchView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        query = self.request.query_params.get('q', None)
+        if query is not None:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(author__username__icontains=query)
+            ).distinct()
+        return queryset.order_by('-created_at')
